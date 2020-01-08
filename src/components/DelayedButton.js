6a52e60cd2d9a93ff61ce7744fa12862657dbea8
lukewaring <lukewaring@gmail.com>
2020-01-08 14:16:24 -0500

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,19 +1,18 @@
 // Hint: If you having trouble with this feature, remember event pooling in React. By the time the setTimeout fires, the event object will have already been returned to the pool. So how can we fix that?
 
-import React from 'react'
+import React from 'react';
 
 class DelayedButton extends React.Component {
+  handleClick = event => {
+    event.persist();
+    window.setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  };
 
-    handleClick = (e) => {
-        e.persist()
-        let arr = [e.clientX, e.clientY]
-        window.setTimeout(() => this.props.onDelayedClick(e), this.props.delay)
-    }
-
-    render() {
-        return <button onClick={this.handleClick}>Delay Test</button>
-    }
-
+  render() {
+    return <button onClick={this.handleClick}>Delayed</button>;
+  }
 }
 
-export default DelayedButton
+export default DelayedButton;
